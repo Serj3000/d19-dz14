9280c39cad0d6ab9f13ff2a637460716056a59ac
Serj3000 <52423079+Serj3000@users.noreply.github.com>
2019-09-15 00:20:13 +0300

    1 commit

---
+++ database/migrations/2019_09_07_154935_create_categories_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->timestamps();
+
+// id BIGINT UNSIGNED AUTO_INCREMENT
+// title CHAR 200 NOT NULL
+// description VARCHAR (100) NOT NULL
+// body TEXT NOT NULL
+// created_at TIMESTAMP
+// updated_at TIMESTAMP
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
